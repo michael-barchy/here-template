5eb341e4567eed74e13a1800f15b63202ec68b13
michael-barchy <michael.barchy@gmail.com>
2026-06-16 23:00:29 +0200

    templates

--- HereTemplate.php
+++ HereTemplate.php
@@ -4,4 +4,70 @@ namespace HereTemplate;
 
 class HereTemplate
 {
+    /** @var string */
+    private $dir = '.';
+    /** @var string */
+    private $cache = 'cache';
+    /**
+     * @param array<string, mixed> $options
+     */
+    public function __construct($options = array())
+    {
+        $dir = $this->dir;
+        if (array_key_exists('dir', $options) && is_string($options['dir'])) {
+            $dir = $options['dir'];
+        }
+        
+        if (is_dir(realpath($dir))) {
+            $this->dir = $dir;
+        }
+        
+        $cache = $this->cache;
+        if (array_key_exists('cache', $options) && is_string($options['cache'])) {
+            $cache = $options['cache'];
+        }
+        
+        if (is_dir(realpath($cache))) {
+            $this->cache = $cache;
+        }
+    }
+    
+    /**
+     * @param string $template
+     * @param array<string, mixed> $context
+     * @return string
+     */
+    public function render($template, $context = array())
+    {
+        // Cache file
+        $templateFile = $this->dir . DIRECTORY_SEPARATOR . $template;
+        $ext = strrchr(basename($templateFile), '.');
+        $name = basename($templateFile, $ext);
+        $hash = md5(file_get_contents($templateFile));
+        $cacheFile = $this->cache . DIRECTORY_SEPARATOR . $name . '.' . $hash . '.php';
+        if (!is_file($cacheFile)) {
+            copy($templateFile, $cacheFile);
+        }
+        
+        // Clear cache
+        $cacheFiles = scandir($this->cache);
+        $cacheFiles = array_filter($cacheFiles, function($f) use ($name, $cacheFile) {
+           return basename($cacheFile) !== $f && $name === substr($f, 0, strlen($name));
+        });
+        foreach ($cacheFiles as $f) {
+            unlink($this->cache . DIRECTORY_SEPARATOR . $f);
+        }
+        
+        ob_start();
+        extract($context);
+        $result = include($cacheFile);
+        if (1 === $result) { // No return statement
+            $result = eval(sprintf(
+                "return <<<HTML\n%s\nHTML;",
+                ob_get_clean()
+            ));
+        }
+        
+        return $result;
+    }
 }
--- README.md
+++ README.md
@@ -47,4 +47,63 @@ require_once('here-template/HereTemplate.php');
 
 use HereTemplate\HereTemplate;
 
+$here = new HereTemplate();
+echo $here->render('templates/welcome.here.php');
+```
 
+### Options
+
+HereTemplate class can be instantiated with the following options array:
+
+* `dir`: default template directory (defaults to current directtory)
+* `cache` : cache folder for compiled templates (defaults to cache/)
+
+```php
+<?php
+
+$here = new HereTemplate(array(
+    'dir' => 'templates',
+    'cache' => sys_get_temp_dir()
+));
+echo $here->render('welcome.here.php');
+```
+
+## Templates
+
+### Template formats
+
+```php
+<?php
+
+$today = date('Y-m-d');
+
+return <<<HTML
+<h1>Today is {$today}</h1>
+HTML;
+```
+
+```php
+<h1>Today is {$today}</h1>
+```
+
+### Context
+
+```php
+<?php
+
+use HereTemplate\HereTemplate;
+
+$here = new HereTemplate();
+echo $here->render('templates/welcome.here.php', array(
+    'today' => date('d/m/Y')
+));
+```
+
+### Protect your templates
+
+Recommended: name your templates with a .here.php extension.
+
+The `return` syntax is not mandatory but avoids execution outside engine context.
+It also protects template source.
+
+You can also create a `.htaccess` file in the your `templates` folder to prevent direct access to template files.
